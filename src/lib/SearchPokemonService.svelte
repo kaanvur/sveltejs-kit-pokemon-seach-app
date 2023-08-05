39aa653a1c47d6475571e5a43968d6cbc3478063
kaanvur <lastcain@gmail.com>
2023-08-06 00:12:24 +0300

    1 file modified

--- src/lib/SearchPokemonService.svelte
+++ src/lib/SearchPokemonService.svelte
@@ -1,6 +1,11 @@
 <script context="module">
   export async function searchPokemon(searchTerm) {
+    /*if(searchTerm == '') {
+      return ['boş']
+    }
+    else {*/
       const response = await fetch(`https://pokedex.bgure.workers.dev/?q=${searchTerm}`);
       return await response.json();
   }
+  //}
 </script>
